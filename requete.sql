5b3595a4bfcefc065edc96adab160e49337850f0
DioufFLR <geoffreyfleur@gmail.com>
2022-12-15 15:23:17 +0100

    1.2

--- requete.sql
+++ requete.sql
@@ -17,3 +17,24 @@ WHERE numcom;
 SELECT COUNT(numcom) AS 'nombre de commande', COUNT(DISTINCT(numfou)) AS 'nombre de fournisseurs'
 FROM entcom;
 
+-- 4
+
+SELECT codart, libart, stkphy, stkale, qteann
+FROM produit
+WHERE stkphy <= stkale AND qteann < 1000;
+
+-- 5
+
+SELECT nomfou AS 'nom du fournisseur', posfou AS 'departement'
+FROM fournis
+WHERE posfou LIKE '75%'
+OR posfou LIKE '78%'
+OR posfou LIKE '92%'
+OR posfou LIKE '77%'
+ORDER BY posfou DESC, nomfou ASC;
+
+-- 6
+
+SELECT datcom AS 'commande passées'
+FROM entcom
+WHERE datcom LIKE '____-03-__%' OR datcom LIKE '____-04-__%';
