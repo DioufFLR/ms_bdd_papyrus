69d667e8252a9acd4259fc3f270da3dd9d4a2646
DioufFLR <geoffreyfleur@gmail.com>
2022-12-15 10:39:41 +0100

    1.1

--- requete.sql
+++ requete.sql
@@ -2,3 +2,18 @@
 
 -- 1
 
+SELECT * 
+FROM `entcom` 
+WHERE numfou = 9120;
+
+-- 2
+
+SELECT numfou AS "code fournisseur" 
+FROM entcom 
+WHERE numcom;
+
+-- 3
+
+SELECT COUNT(numcom) AS 'nombre de commande', COUNT(DISTINCT(numfou)) AS 'nombre de fournisseurs'
+FROM entcom;
+
